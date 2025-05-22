f34451327dce31d4060326836de3b9701b8ad695
JasonAlbertEinstien <j2004nol@gmail.com>
2025-05-22 20:24:52 +0800

    CNN init

--- cnn.py
+++ cnn.py
@@ -1,7 +1,56 @@
 # CNN from scratch
 import numpy as np
+
+## We build convulution 2D 
+# Alex Net
 class CNN:
     def __init__(self):
+        self.conv1 = Conv2D()
+        self.relu1 = LeakyRELU()
+        self.pool1 = MaxPool2D()
+        self.conv2 = Conv2D()
+        self.relu2 = LeakyRELU()
+        self.pool2 = MaxPool2D()
+        self.conv3 = Conv2D()
+        self.conv4 = Conv2D()
+        self.conv5 = Conv2D()
+        self.relu3 = LeakyRELU()
+        self.pool3 = MaxPool2D()
+        self.fc1 = Linear()
+        self.fc2 = Linear()
+        self.fc3 = Linear()
+
+# There are three eleemnt for a CNN neural network 
+# Convoulution Layer , Pooling Layer and Fully Connected Layer
+# 
+# The difficult part would be how to do the back propagation with CNN  
+
+class Conv2D:
+    """
+        N: batch size 
+        C_in: Number of channel
+        H: height input 
+        W: width input 
+
+        Expected Outpu Dimension: 
+        H_out: floor(H_in + 2 x padding[0] - dilation[0] x (kernel[0] -1) -1)/ stride[0] +  1
+    """
+    def __init__(self , n:int , c:int , h:int , w:int , stride:int = 0 , padding:int = 0):
         pass
 
+    def forward(self):
+        pass 
+
+
+class MaxPool2D:
+    pass 
+
+class Linear:
+    pass 
+
+class LeakyRELU:
+    pass 
+
+class Softmax:
+    pass 
 
--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
-This project aims build CNN netowrk from scretc with only numpy.
+# Deep Learning from scretch
+This project aims build Nerual Netowrk netowrk from scretc with only numpy.
+Build your own deep learning network is the simpliest way to learn what is deep learning and neural network. Unlike other subjectes ,currently there are limited resources/book exercies for us to understand nerual network. This repo is built for myself to build everything from scretech such that I could understand how it works behind. 
+
+This is far from efficienct compare to how it done in pytorch, yet I do think it is far enough for me to get used to the logic behind deep learning.
+
+If this repo can help you to learn don't forget to give me a little star. 
