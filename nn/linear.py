db8aa23f35f35a798d786d51e96a4ca05e3feea1
JasonAlbertEinstien <j2004nol@gmail.com>
2025-05-24 16:22:57 +0800

    refactor

--- nn/linear.py
+++ nn/linear.py
@@ -4,5 +4,29 @@
 import numpy as np
 
 class Linear:
-    def __init__(self):
-        pass
+    def __init__(self , input_dim , output_dim):
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.weight = np.random.randn(input_dim , output_dim)
+        self.x = None
+        self.o = None
+    
+    def forward(self ,x):
+        self.x =x 
+        self.o = np.einsum('ni,ij->nj' , x , self.weight)
+        return self.o
+
+
+    def backward(self ,loss):
+        """
+            loss dimensiont: [N , o]
+
+            backward return dl/dw , dl/dx in order
+            dl/dw = dl/do * do/dw = dl/do * x
+            dl/dx = dl/do * do/dx = dl/do * w 
+
+        """
+        dw = np.einsum('no, ni -> io' , loss , self.x)
+        dx = np.einsum('no, oj -> nj ' , loss , self.weight.T)
+        return dw , dx
+
--- readme.md
+++ readme.md
@@ -16,3 +16,37 @@ Todo List:
 
     - [ ] Vision Transformer (if available)
 - [ ] RNN & LSTM
+
+## How to use this library 
+
+### Linear
+To use linear layer we can call
+```py
+from nn.linear import Linear
+import numpy as np
+
+linear = Linear(input_dim=2, output_dim=1)
+
+x = np.array([[1.0, 2.0],
+                [3.0, 4.0],
+                [5.0, 6.0]])
+
+y_true = np.array([[1.0], [2.0], [3.0]])
+
+y_pred = linear.forward(x)
+
+loss = np.mean((y_pred - y_true) ** 2)
+grad_loss_wrt_output = 2 * (y_pred - y_true) / y_true.shape[0]
+
+print(f"Loss before update: {loss:.4f}")
+
+grad_w, grad_x = linear.backward(grad_loss_wrt_output)
+
+learning_rate = 0.01
+linear.weight -= learning_rate * grad_w
+
+y_pred_after = linear.forward(x)
+loss_after = np.mean((y_pred_after - y_true) ** 2)
+print(f"Loss after update: {loss_after:.4f}")
+
+```
