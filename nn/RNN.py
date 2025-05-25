25e783de8394aa2e7f63f0dbdc06bbd166168682
JasonAlbertEinstien <j2004nol@gmail.com>
2025-05-25 20:19:56 +0800

    RNN LSTM

--- nn/RNN.py
+++ nn/RNN.py
@@ -1,6 +1,6 @@
 import numpy as np 
 
-from nn.activation import Tanh
+from nn.activation import Tanh , Sigmoid
 
 class RNN:
     def __init__(self , input_dim , hidden_dim , output_dim):
@@ -34,12 +34,95 @@ class RNN:
 
 class LSTM:
     """
-        The key difference between LSTM and simple RNN is instead of directly use tanh 
-        they would use:
-            - forget gate 
-            - Input gate
-            - Output gate
-            - Cell memory
+        a simple RNN would takes an input and the previous hidden state
+        then it would output a new hidden state 
+
+        -- it is simple but would forget previous information due to vanishing gradients
+
+        LSTM: 
+            introduce cell state and 
+                - forget gate: decides what to discard from the previous cell state 
+                - input state: determines what new information to add to the cell state.
+                - output gate: controls what to output based on the current cell state.
+                - cell memory: A "conveyor belt" that carries information across time step
+
     """
-    def __init__(self):
-        pass
+    def __init__(self , input_dim , output_dim , output_size , lr=0.01):
+        """
+            Initialize the LSTM with input and output dimensions:
+
+            Args:
+                input_dim (int): Size of the input vector
+                output_dim (int): size of the hidden state
+
+            Expected:
+                Initalize all weight values
+        """
+        self.input_dim = input_dim
+        self.hidden_dim = output_dim
+        self.output_size = output_size
+        self.lr = lr 
+
+        # input [h_prev , x]  --> [h_prev +x]
+        concate_dim = self.hidden_dim + self.input_dim
+        
+        # forget gate 
+        self.Wf = np.random.randn(self.hidden_dim , concate_dim) * 0.01
+        self.bf = np.zeros(shape=(self.hidden_dim , 1))
+
+        self.Wi = np.random.randn(self.hidden_dim  , concate_dim) * 0.01
+        self.bi = np.zeros((self.hidden_dim , 1))
+
+        self.Wc = np.random.randn(self.hidden_dim , concate_dim)*0.01
+        self.bc = np.zeros((self.hidden_dim , 1))
+
+        self.Wo = np.random.randn(self.hidden_dim , concate_dim) * 0.01
+        self.bo = np.zeros((self.hidden_dim , 1))
+
+        # Initialize weight for output layer
+        self.W_out = np.random.randn(self.output_size , self.hidden_dim) * 0.01
+        self.b_out = np.random.zeros((self.output_size))
+
+        # set h_0 and c_0
+        self.h = np.zeros((self.hidden_dim , 1))
+        self.c = np.zeros((self.hidden_dim , 1))
+
+    def forward(self , x):
+        """
+            forward pass for one time step
+        """
+        concact = np.vstack((self.h , x))
+        
+        # forget gate 
+        f = Sigmoid().forward(np.dot(self.Wf , concact) + self.bf)
+        
+        # input gate
+        i = Sigmoid().forward(np.dot(self.Wi , concact) + self.bi)
+
+        c_tilde = Tanh().forward(np.dot(self.Wc , concact) + self.bc)
+
+        # forget + new info
+        self.c = f * self.c + i * c_tilde
+
+        o = Sigmoid().forward(np.dot(self.Wo , concact) + self.bo) # we should return this ? 
+
+        self.h = o * np.tanh(self.c)
+        # we have to do some chaching here 
+        y_t = np.dot(self.W_out , self.h) + self.b_out
+        return self.h 
+
+
+    def backward(self ,loss):
+        """
+            Args:
+                loss: dl/dy
+
+            backward: 
+                we have to find 
+                    dl/dw_f 
+                    dl/dw_o
+                    dl/dw_c
+        """
+        # first we have to find dl/dh = dl/dy * dy/dh 
+
+        #TODO backpropagation to find the gradient
